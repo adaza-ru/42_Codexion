f8dd9d5c0e5308d163946334867454b10f929249
adaza-ru <adaza-ru@student.42malaga.com>
2026-05-28 18:45:53 +0200

    starting

---
+++ h_codexion.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   h_codexion.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaza-ru <adaza-ru@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 18:22:22 by adaza-ru          #+#    #+#             */
+/*   Updated: 2026/05/28 18:30:02 by adaza-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef H_CODEXION
+# define H_CODEXION
+
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adaza-ru <adaza-ru@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 18:26:06 by adaza-ru          #+#    #+#             */
+/*   Updated: 2026/05/28 18:44:47 by adaza-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "h_codexion.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc != 9 || check_args_errors(argv) == 1)
+		return 1;
+	parse_args();
+	start_simulation();
+	return 0;
+}
