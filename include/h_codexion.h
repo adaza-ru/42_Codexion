f398b7d872b0b1c1b42830cbf08c1bbeafca3eb1
adaza-ru <zzantoniodaza@hotmail.es>
2026-07-14 00:29:18 +0200

    add: ansii escape codes to .h

--- include/h_codexion.h
+++ include/h_codexion.h
@@ -6,7 +6,7 @@
 /*   By: adaza-ru <adaza-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 23:00:46 by adaza-ru          #+#    #+#             */
-/*   Updated: 2026/07/13 23:02:55 by adaza-ru         ###   ########.fr       */
+/*   Updated: 2026/07/14 00:05:06 by adaza-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,25 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <string.h>
+
+# ifdef CONFIG_COLOR
+#  define RESET          "\033[0m"
+#  define TIMESTAMP_CLR  "\033[90m"
+#  define ID_CLR         "\033[36m"
+#  define DONGLE_CLR     "\033[33m"
+#  define COMPILE_CLR    "\033[32m"
+#  define DEBUG_CLR      "\033[34m"
+#  define REFACTOR_CLR   "\033[35m"
+#  define DEATH_CLR      "\033[31m"
+# else
+#  define RESET          ""
+#  define TIMESTAMP_CLR  ""
+#  define ID_CLR         ""
+#  define DONGLE_CLR     ""
+#  define COMPILE_CLR    ""
+#  define DEBUG_CLR      ""
+#  define REFACTOR_CLR   ""
+#  define DEATH_CLR      ""
+# endif
 
 #endif
